ed0d5d33a9c0ae6f0b2b4e525833e72fe2933bbc
emily-08 <applesuzuki08@iCloud.com>
2021-03-17 16:28:07 +0900

    訂正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
     def show
-      @user = User.find(:id)
-      @name = @user.name
-      @profile = @user.profile
-      @occupation = @user.occupation
-      @position = @user.position
+      @user = User.find(params[:id])
+      @name = user.name
+      @profile = user.profile
+      @occupation = user.occupation
+      @position = user.position
     end
 end
